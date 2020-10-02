b6f447fe4a832e3c0fbcaa9dd2b80d0b4eaa24d5
RDP77 <ravidwiputra@gmail.com>
2020-10-02 14:26:05 +0700

    Changes Storage

--- resources/views/components/feature-package.blade.php
+++ resources/views/components/feature-package.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-4">
             <div class="py-5 rounded shadow"
-                style="background: url('{{ asset('storage/images/halus.png') }}') top center/cover;">
+                style="background: url('{{ asset('storage/images/package.jpg') }}') top center/cover;">
                 <div class="p-4">
                     <h3>Paket <br> Lembut Dihati</h3>
                     <a href="{{ route('shop') }}" class="btn btn-sm btn-soft-primary mt-2">Beli Sekarang</a>
@@ -13,7 +13,7 @@
 
         <div class="col-md-4 mt-4 pt-2 mt-sm-0 pt-sm-0">
             <div class="py-5 rounded shadow"
-                style="background: url('{{ asset('storage/images/halus.png') }}') top center/cover;">
+                style="background: url('{{ asset('storage/images/package.jpg') }}') top center/cover;">
                 <div class="p-4">
                     <h3>Paket <br> Sayang Selalu</h3>
                     <a href="{{ route('shop') }}" class="btn btn-sm btn-soft-primary mt-2">Beli Sekarang</a>
@@ -24,7 +24,7 @@
 
         <div class="col-md-4 mt-4 pt-2 mt-sm-0 pt-sm-0">
             <div class="py-5 rounded shadow"
-                style="background: url('{{ asset('storage/images/halus.png') }}') top center/cover;">
+                style="background: url('{{ asset('storage/images/package.jpg') }}') top center/cover;">
                 <div class="p-4">
                     <h3>Paket <br> Terima Kasih</h3>
                     <a href="{{ route('shop') }}" class="btn btn-sm btn-soft-primary mt-2">Beli Sekarang</a>
